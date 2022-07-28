432ba11c6c9a5e340a8b1d2c6bf8d1c768dc13aa
imnikhilanand <imnikhilanand@gmail.com>
2022-07-27 23:16:27 -0400

    Added analysis code

---
+++ analysis.R
@@ -0,0 +1,10 @@
+
+# Importing the packages
+library('survival')
+library('survminer')
+
+# importing the dataset from the package
+data(cancer, package="survival")
+
+# viewing the dataset
+head(gbsg)
