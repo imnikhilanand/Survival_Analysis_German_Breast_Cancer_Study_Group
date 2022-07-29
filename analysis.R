d81cbf2b4eb46512657fbe9ec79cf9d2f74a2dbd
imnikhilanand <imnikhilanand@gmail.com>
2022-07-28 23:59:15 -0400

    Added modeling

--- analysis.R
+++ analysis.R
@@ -8,3 +8,13 @@ data(cancer, package="survival")
 
 # viewing the dataset
 head(gbsg)
+
+# modeling the data
+res.cox <- coxph(Surv(rfstime,status)~age+meno+size+grade+nodes+pgr+er+hormon, data=gbsg)
+
+# fro
+
+
+
+
+
